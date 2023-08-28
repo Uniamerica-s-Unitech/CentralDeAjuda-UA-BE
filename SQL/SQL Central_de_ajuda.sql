31bf1cbbf02f74f9de2acd06e74c3b8cba87793f
hesham esbier <heshamgamer2015@gmail.com>
2023-08-28 20:45:38 -0300

    SQL

---
+++ SQL/SQL Central_de_ajuda.sql
@@ -0,0 +1,33 @@
+create table marca(
+	id serial primary key,
+	nome varchar(50)
+)
+
+create table modelo(
+	id serial primary key,
+	nome varchar(50),
+	marca_id integer references marca(id) 
+)
+
+create table notebooks (
+	id serial primary key,
+	patrimonio varchar (50),
+	modelo_id integer references modelo(id)
+)
+
+create table aluno (
+	id serial primary key,
+	nome varchar(100),
+	RA integer unique
+)
+
+create table ticket(
+	id serial primary key,
+	aluno_id integer references aluno(id),
+	patrimonio_id integer references notebooks(id),
+	data_de_entrega timestamp,
+	data_de_devolucao timestamp,
+	comentrio varchar(250)
+)
+
+
