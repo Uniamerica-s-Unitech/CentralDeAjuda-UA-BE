bf64521ffdba3c13a3bb6f1bbb95ebdcb3ac8436
homamalsuyoufi <homamalsuyoufi@gmail.com>
2023-08-28 19:52:39 -0300

    padrao

---
+++ src/main/java/uniamerica/centralDeAjuda/CentralDeAjudaApplication.java
@@ -0,0 +1,13 @@
+package uniamerica.centralDeAjuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDeAjudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDeAjudaApplication.class, args);
+	}
+
+}
