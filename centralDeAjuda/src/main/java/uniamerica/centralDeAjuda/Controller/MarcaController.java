1042dc79a674a3ca6ffaba62f57119035a346c19
HishamEsbier <heshamgamer2015@gmail.com>
2023-08-31 01:52:46 -0300

    1

--- centralDeAjuda/src/main/java/uniamerica/centralDeAjuda/Controller/MarcaController.java
+++ centralDeAjuda/src/main/java/uniamerica/centralDeAjuda/Controller/MarcaController.java
@@ -1,4 +1,30 @@
 package uniamerica.centralDeAjuda.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import uniamerica.centralDeAjuda.DTO.MarcaDTO;
+import uniamerica.centralDeAjuda.Services.MarcaService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/marca")
 public class MarcaController {
+    @Autowired
+    private MarcaService marcaService;
+
+    @GetMapping("/lista")
+    public List<MarcaDTO> listar(){
+        return marcaService.listar();
+    }
+    @GetMapping("/{id}")
+    public MarcaDTO findById(@PathVariable Long id){
+        return marcaService.findById(id);
+    }
+    @PostMapping
+    public MarcaDTO cadastar(@RequestBody MarcaDTO marcaDTO){
+        return marcaService.cadastar(marcaDTO);
+    }
+
+
 }
